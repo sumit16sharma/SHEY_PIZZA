6a2d21104d584750a09a80318ea61457cd4108c3
Sumit Sharma <sumit.16noida@gmail.com>
2021-09-25 17:47:21 +0530

    Filter Component Logic

--- client/src/actions/pizzaActions.js
+++ client/src/actions/pizzaActions.js
@@ -16,3 +16,24 @@ export const getAllPizzas = () => async (dispatch) => {
     dispatch({ type: GET_PIZZAS_FAIL, payload: error });
   }
 };
+
+export const filterPizzas = (searchKey, category) => async (dispatch) => {
+  try {
+    dispatch({ type: GET_PIZZAS_REQUEST });
+
+    const { data } = await axios.get('/api/pizzas/getAllPizzas');
+    var filteredPizzas = data.filter((pizza) =>
+      pizza.name.toLowerCase().includes(searchKey)
+    );
+
+    if (category !== 'all') {
+      var filteredPizzas = data.filter(
+        (pizza) => pizza.category.toLowerCase() == category
+      );
+    }
+
+    dispatch({ type: GET_PIZZAS_SUCCESS, payload: filteredPizzas });
+  } catch (error) {
+    dispatch({ type: GET_PIZZAS_FAIL, payload: error });
+  }
+};
--- client/src/components/Filter.js
+++ client/src/components/Filter.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { filterPizzas } from '../actions/pizzaActions';
 
 const Filter = () => {
   const dispatch = useDispatch();
+  const [searchKey, setSearchKey] = useState('');
+  const [category, setCategory] = useState('all');
 
   return (
     <div className='container'>
@@ -12,17 +15,30 @@ const Filter = () => {
             type='text'
             className='form-cantrol w-100'
             placeholder='Search Pizzas'
+            value={searchKey}
+            onChange={(e) => setSearchKey(e.target.value)}
           />
         </div>
         <div className='col-md-3 w-5'>
-          <select className='form-control form-control-sm w-100 mt-2'>
+          <select
+            className='form-control form-control-sm w-100 mt-2'
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          >
             <option value='all'>All</option>
             <option value='veg'>Veg</option>
             <option value='nonveg'>Non-Veg</option>
           </select>
         </div>
         <div className='col-md-3 w-5'>
-          <button className='btn w-100 mt-1'>Filter</button>
+          <button
+            className='btn w-100 mt-1'
+            onClick={() => {
+              dispatch(filterPizzas(searchKey, category));
+            }}
+          >
+            Filter
+          </button>
         </div>
       </div>
     </div>
