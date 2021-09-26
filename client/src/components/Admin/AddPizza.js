1679a79348d0cd242e7520593ea9ca8d03a06fd1
Sumit Sharma <sumit.16noida@gmail.com>
2021-09-26 13:38:27 +0530

    Add Pizza Screen Design

--- client/src/components/Admin/AddPizza.js
+++ client/src/components/Admin/AddPizza.js
@@ -1,9 +1,105 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const AddPizza = () => {
+  const [name, setName] = useState('');
+  const [smallPrice, setSmallPrice] = useState();
+  const [mediumPrice, setMediumPrice] = useState();
+  const [largePrice, setLargePrice] = useState();
+  const [image, setImage] = useState('');
+  const [description, setDescription] = useState('');
+  const [category, setCategory] = useState('');
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    const pizza = {
+      name,
+      prices: {
+        small: Number(smallPrice),
+        medium: Number(mediumPrice),
+        large: Number(largePrice),
+      },
+      image,
+      description,
+      category,
+    };
+
+    console.log(pizza);
+  };
+
   return (
     <div>
-      <h1>Add Pizza</h1>
+      <div className='text-start'>
+        <h1>Add Pizza</h1>
+        <form onSubmit={(e) => onSubmit(e)}>
+          <div class='form-group'>
+            <input
+              type='text'
+              class='form-control'
+              placeholder='Name'
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div class='form-group'>
+            <input
+              type='text'
+              class='form-control'
+              placeholder='Small Variant Price'
+              value={smallPrice}
+              onChange={(e) => setSmallPrice(e.target.value)}
+            />
+          </div>
+          <div class='form-group'>
+            <input
+              type='text'
+              class='form-control'
+              placeholder='Medium Variant Price'
+              value={mediumPrice}
+              onChange={(e) => setMediumPrice(e.target.value)}
+            />
+          </div>
+          <div class='form-group'>
+            <input
+              type='text'
+              class='form-control'
+              placeholder='Large Variant Price'
+              value={largePrice}
+              onChange={(e) => setLargePrice(e.target.value)}
+            />
+          </div>
+          <div class='form-group'>
+            <input
+              type='text'
+              class='form-control'
+              placeholder='Image URL'
+              value={image}
+              onChange={(e) => setImage(e.target.value)}
+            />
+          </div>
+          <div class='form-group'>
+            <input
+              type='text'
+              class='form-control'
+              placeholder='Description..'
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+          </div>
+          <div class='form-group'>
+            <input
+              type='text'
+              class='form-control'
+              placeholder='Category'
+              value={category}
+              onChange={(e) => setCategory(e.target.value)}
+            />
+          </div>
+          <button type='submit' class='btn btn-primary mt-3'>
+            Submit
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
